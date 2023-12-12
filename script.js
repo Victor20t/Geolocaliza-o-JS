ec6fd915d93ddb1701cc4a27d27c8aded6659eab
Victor Gabriel <gabrielcaruarulor@gmail.com>
2023-12-12 00:37:10 -0300

    implementação do mapa

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
-
-let h2 = document.querySelector('h2');
+var h2 = document.querySelector('h2');
+var map;
 
 function success(pos){
     console.log(pos.coords.latitude, pos.coords.longitude);
-    h2.textContent = `Latitude:${pos.coords.latitude}, Longitude:${pos.coords.longitude}`;
+    h2.textContent = `Latitude: ${pos.coords.latitude}, Longitude: ${pos.coords.longitude}`;
+
+    if (!map) {
+        map = L.map('map').setView([pos.coords.latitude, pos.coords.longitude], 13);
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        L.marker([pos.coords.latitude, pos.coords.longitude]).addTo(map)
+            .bindPopup('Localização ')
+            .openPopup();
+    } else {
+        // Atualizar a visualização do mapa, se necessário
+        map.setView([pos.coords.latitude, pos.coords.longitude], 13);
+    }
 }
 
 function error(err){
     console.log(err);
 }
 
-let watchID = navigator.geolocation.watchPosition(success, error, {
+var watchID = navigator.geolocation.watchPosition(success, error, {
     enableHighAccuracy: true,
     timeout: 5000
 });
-
-//navigator.geolocation.clearWatch(watchID);
